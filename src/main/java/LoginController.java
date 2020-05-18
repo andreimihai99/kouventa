1806c5ee2be59fd0c65b4b4aee4ac5a1ac6903c5
Andrei Mihai <andreimihai99@yahoo.com>
2020-05-18 21:02:43 +0300

    Implemented login feature task: AF-8

--- src/main/java/LoginController.java
+++ src/main/java/LoginController.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+    public Button loginButton;
+    public PasswordField passwordField;
+    public TextField usernameField;
+    public Button loginRegisterButton;
+    public Button loginRegisterAdminButton;
+
+    public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    public void handleLoginButton(){
+    }
+
+    public void handleLoginRegisterButton(){
+    }
+
+    public void handleLoginRegisterAdminButton(){
+    }
 }
