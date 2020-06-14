1872c666f31f322c6d83e04cee71b01ad9beb858
madalin-minea <madalin.minea@ligaac.ro>
2020-06-14 18:02:26 +0300

    Am terminat de implementat si functiile de stergere si schimbare a categoriilor

--- src/main/java/Admin.java
+++ src/main/java/Admin.java
@@ -1,16 +1,18 @@
 
 import java.io.*;
-//import java.nio.file.Files;
-//import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.Security;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
 import java.util.Base64;
+import java.util.EventObject;
 import java.util.Iterator;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -29,10 +31,16 @@ import org.json.simple.parser.ParseException;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+
+import static java.util.Collections.sort;
+
 public class Admin extends Application {
 
     @FXML
     private TextField category;
+    @FXML
+    private TextField changeInTxt;
+    static String change=new String();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -88,20 +96,107 @@ public class Admin extends Application {
             JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader("src/main/resources/Categories.json"));
             JSONArray jsonArray = (JSONArray) jsonObject.get("Category");
 
-            jsonArray.add(category.getText());
+            if (jsonArray.contains(category.getText())) {
+                System.out.println("Deja exista categoria");
+            } else {
+                jsonArray.add(category.getText());
+                sort(jsonArray);
 
-            try {
+                try {
+                    FileWriter file = new FileWriter("src/main/resources/Categories.json");
+                    objfin.put("Category", jsonArray);
+                    file.write(objfin.toJSONString());
+                    file.close();
+
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+            }
+            } catch(FileNotFoundException e){
+                e.printStackTrace();
+            } catch(IOException e)
+            {
+                e.printStackTrace();
+            } catch(ParseException e)
+            {
+                e.printStackTrace();
+            }
+
+    }
+    @FXML
+    private void clickDelete(ActionEvent event) throws IOException {
+        JSONParser jsonParser = new JSONParser();
+        try {
+
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader("src/main/resources/Categories.json"));
+            JSONArray jsonArray = (JSONArray) jsonObject.get("Category");
+
+            if(jsonArray.contains(category.getText()))
+            {
+                jsonArray.remove(category.getText());
+
+                JSONObject objfin = new JSONObject();
                 FileWriter file = new FileWriter("src/main/resources/Categories.json");
                 objfin.put("Category",jsonArray);
                 file.write(objfin.toJSONString());
                 file.close();
 
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
             }
+            else
+            {
 
+                System.out.println("Nu exista categoria");
+            }
+        } catch (FileNotFoundException e)  {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        } catch (ParseException e)
+        {
+            e.printStackTrace();
+        }
 
+    }
+
+    @FXML
+    private void clickChange(ActionEvent event) throws IOException {
+        JSONParser jsonParser = new JSONParser();
+
+        try {
+
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader("src/main/resources/Categories.json"));
+            JSONArray jsonArray = (JSONArray) jsonObject.get("Category");
+
+            if(jsonArray.contains(category.getText()))
+            {
+                change=category.getText();
+                Stage newStage =new Stage();
+                Node  source = (Node)  event.getSource();
+                Stage stage  = (Stage) source.getScene().getWindow();
+
+                // Create the FXMLLoader
+                FXMLLoader loader = new FXMLLoader();
+                // Path to the FXML File
+                String fxmlDocPath = "src/main/resources/ChangeIn.fxml";
+                FileInputStream fxmlStream = new FileInputStream(fxmlDocPath);
+
+                // Create the Pane and all Details
+                Pane root = (Pane) loader.load(new FileInputStream(fxmlDocPath));
+
+                // Create the Scene
+
+                Scene scene = new Scene(root);
+                newStage.setScene(scene);
+
+                newStage.show();
+            }
+            else
+            {
+                System.out.println("Nu exista categoria");
+            }
         } catch (FileNotFoundException e)  {
             e.printStackTrace();
         } catch (IOException e)
@@ -112,12 +207,44 @@ public class Admin extends Application {
             e.printStackTrace();
         }
     }
-    @FXML
-    private void clickDelete(ActionEvent e) throws IOException {
 
-
-    }
     @FXML
-    private void clickChange(ActionEvent e) throws IOException {
+    private void clickChangeIn(ActionEvent event) throws IOException
+    {
+        JSONParser jsonParser = new JSONParser();
+        try {
+
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader("src/main/resources/Categories.json"));
+            JSONArray jsonArray = (JSONArray) jsonObject.get("Category");
+
+            if(jsonArray.contains(changeInTxt.getText()))
+            {
+                System.out.println("Exista deja");
+            }
+            else
+            {
+                jsonArray.remove(change);
+
+                jsonArray.add(changeInTxt.getText());
+                sort(jsonArray);
+                JSONObject objfin = new JSONObject();
+                FileWriter file = new FileWriter("src/main/resources/Categories.json");
+                objfin.put("Category",jsonArray);
+                file.write(objfin.toJSONString());
+                file.close();
+                Node  source = (Node)  event.getSource();
+                Stage stage  = (Stage) source.getScene().getWindow();
+                stage.close();
+
+            }
+        } catch (FileNotFoundException e)  {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        } catch (ParseException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
